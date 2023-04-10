928ac79738fcbe09a0f961aa2675529d890a1def
maulish34 <c2045137@ncl.ac.uk>
2023-04-10 20:23:11 +0100

    Updated main with stocks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import angel_one_api_connect as api
 
 def main():
-    print(api.Run("2021-04-06 00:00", "2021-04-14 00:00"))
+    stocks = ['SBIN', 'SRF', 'KTKBANK']
+    print(api.Run(stocks, "2021-12-06 00:00", "2021-12-15 00:00"))
 
 if __name__ == "__main__":
     main()
