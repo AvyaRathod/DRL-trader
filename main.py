6b192f09b3954086cf332769dd5c1d7d60fb851b
maulish34 <c2045137@ncl.ac.uk>
2023-06-28 12:24:21 +0100

    Added login details and tested fetching data

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import angel_one_api_connect as api
 
 def main():
+
+    dh = api.DataHandling()
+    dh.Login()
+    dh.initializeTokenMap()
+
+
     stocks = ['SBIN', 'SRF', 'KTKBANK']
-    print(api.Run(stocks, "2021-12-06 00:00", "2021-12-15 00:00"))
+    print(api.DataHandling.dataDownloader(stocks, "2021-12-06 00:00", "2021-12-15 00:00"))
 
 if __name__ == "__main__":
     main()
